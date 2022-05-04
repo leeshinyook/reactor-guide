68b9a3b237bd58f8e6791e7896e2a2a388c511c8
leeshinyook <tlsdbr97@gmail.com>
2022-05-04 16:03:54 +0900

    [test:part2] static factories on mono test

--- src/test/kotlin/me/shinyook/reactorguide/Part02MonoTest.kt
+++ src/test/kotlin/me/shinyook/reactorguide/Part02MonoTest.kt
@@ -1,5 +1,41 @@
 package me.shinyook.reactorguide
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import reactor.test.StepVerifier
+import java.time.Duration
 
-internal class Part02MonoTest
+internal class Part02MonoTest {
+    private val guideLine = Part02Mono()
+
+    @Test
+    fun empty() {
+        val mono = guideLine.emptyMono()
+        StepVerifier.create(mono)
+            .verifyComplete()
+    }
+
+    @Test
+    fun noSignal() {
+        val mono = guideLine.monoWithNoSignal()
+        StepVerifier.create(mono)
+            .expectSubscription()
+            .expectNoEvent(Duration.ofSeconds(1))
+            .thenCancel()
+            .verify()
+    }
+
+    @Test
+    fun emitFoo() {
+        val mono = guideLine.fooMono()
+        StepVerifier.create(mono)
+            .expectNext("foo")
+            .verifyComplete()
+    }
+
+    @Test
+    fun error() {
+        val mono = guideLine.errorMono()
+        StepVerifier.create(mono)
+            .verifyError(IllegalStateException().javaClass)
+    }
+}
