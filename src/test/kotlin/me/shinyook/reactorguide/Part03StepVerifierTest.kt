bf1ba91c5dcae8f428451cfe6a6ee6a6931e1ade
leeshinyook <tlsdbr97@gmail.com>
2022-05-04 17:06:16 +0900

    [test:part3] stepverifier test

--- src/test/kotlin/me/shinyook/reactorguide/Part03StepVerifierTest.kt
+++ src/test/kotlin/me/shinyook/reactorguide/Part03StepVerifierTest.kt
@@ -1,5 +1,37 @@
 package me.shinyook.reactorguide
 
-import org.junit.jupiter.api.Assertions.*
+import me.shinyook.reactorguide.domain.User
+import org.junit.jupiter.api.Test
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import java.time.Duration
 
-internal class Part03StepVerifierTest
+internal class Part03StepVerifierTest {
+    private val guideLine = Part03StepVerifier()
+
+    @Test
+    fun expectFooBarComplete() {
+        guideLine.expectFooBarComplete(Flux.just("foo", "bar"))
+    }
+
+    @Test
+    fun expectFooBarError() {
+        guideLine.expectFooBarError(Flux.just("foo", "bar").concatWith(Mono.error(RuntimeException())))
+    }
+
+    @Test
+    fun expectSkylerJesseComplete() {
+        guideLine.expectSkylerJesseComplete(Flux.just(User.SKYLER, User.JESSE))
+    }
+
+    @Test
+    fun expect10Elements() {
+        guideLine.expect10Elements(Flux.range(1, 10))
+    }
+
+    @Test
+    fun expect3600Elements() {
+        guideLine.expect3600Elements { Flux.interval(Duration.ofSeconds(1)).take(3600) }
+    }
+
+}
