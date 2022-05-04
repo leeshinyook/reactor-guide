953187a2065bbdbd9572a6d3c0f706c2f29894e5
leeshinyook <tlsdbr97@gmail.com>
2022-05-04 17:41:22 +0900

    [test:part4] asynchronous transform test

--- src/test/kotlin/me/shinyook/reactorguide/Part04TransformTest.kt
+++ src/test/kotlin/me/shinyook/reactorguide/Part04TransformTest.kt
@@ -1,5 +1,44 @@
 package me.shinyook.reactorguide
 
-import org.junit.jupiter.api.Assertions.*
+import me.shinyook.reactorguide.domain.User
+import me.shinyook.reactorguide.repository.UserRepository
+import org.junit.jupiter.api.Test
+import reactor.test.StepVerifier
 
-internal class Part04TransformTest
+internal class Part04TransformTest {
+
+    private val guideLine = Part04Transform()
+    private val userRepository = UserRepository()
+
+    @Test
+    fun transformMono() {
+        val user = userRepository.findFirst()
+        StepVerifier.create(guideLine.capitalizeOne(user))
+            .expectNext(User.JESSE.toUppercase())
+            .verifyComplete()
+    }
+
+    @Test
+    fun transformFlux() {
+        val users = userRepository.findAll()
+        StepVerifier.create(guideLine.capitalizeMany(users))
+            .expectNext(User.JESSE.toUppercase(),
+                User.SKYLER.toUppercase(),
+                User.SAUL.toUppercase(),
+                User.WALTER.toUppercase()
+            )
+            .verifyComplete()
+    }
+
+    @Test
+    fun asyncTransformFlux() {
+        val users = userRepository.findAll()
+        StepVerifier.create(guideLine.asyncCapitalizeMany(users))
+            .expectNext(User.JESSE.toUppercase(),
+                User.SKYLER.toUppercase(),
+                User.SAUL.toUppercase(),
+                User.WALTER.toUppercase()
+            )
+            .verifyComplete()
+    }
+}
