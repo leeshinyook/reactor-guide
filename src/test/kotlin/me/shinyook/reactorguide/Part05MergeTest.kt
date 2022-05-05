e36b85c983def55066a453c39dba7381b7463b62
leeshinyook <tlsdbr97@gmail.com>
2022-05-05 23:45:08 +0900

    [test:part5] flux, mono merge test with delay repository

--- src/test/kotlin/me/shinyook/reactorguide/Part05MergeTest.kt
+++ src/test/kotlin/me/shinyook/reactorguide/Part05MergeTest.kt
@@ -1,5 +1,38 @@
 package me.shinyook.reactorguide
 
-import org.junit.jupiter.api.Assertions.*
+import me.shinyook.reactorguide.domain.User
+import me.shinyook.reactorguide.repository.UserRepository
+import org.junit.jupiter.api.Test
+import reactor.test.StepVerifier
 
-internal class Part05MergeTest
+internal class Part05MergeTest {
+
+    private val guideLine = Part05Merge()
+    private val userRepository = UserRepository()
+
+    @Test
+    fun mergeFluxWithInterleave() {
+        val flux = guideLine.mergeFluxWithInterleave(userRepository.findAllWithDelay(500), userRepository.findAll())
+        StepVerifier.create(flux)
+            .expectNext(User.JESSE, User.SKYLER, User.SAUL, User.WALTER, User.MIKE, User.MARIE)
+            .verifyComplete()
+    }
+
+    @Test
+    fun mergeFluxWithNoInterleave() {
+        val flux = guideLine.mergeFluxWithNoInterleave(userRepository.findAllWithDelay(500), userRepository.findAll())
+        StepVerifier.create(flux)
+            .expectNext(User.MIKE, User.MARIE, User.JESSE, User.SKYLER, User.SAUL, User.WALTER)
+            .verifyComplete()
+    }
+
+    @Test
+    fun createFluxFromMultipleMono() {
+        val mikeMono = userRepository.findFirstWithDelay(300)
+        val jesseMono = userRepository.findFirst()
+        val flux = guideLine.createFluxFromMultipleMono(mikeMono, jesseMono)
+        StepVerifier.create(flux)
+            .expectNext(User.MIKE, User.JESSE)
+            .verifyComplete()
+    }
+}
