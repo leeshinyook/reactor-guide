406d1010e0ba5068ef404df8497092db8bc4c852
leeshinyook <tlsdbr97@gmail.com>
2022-05-06 00:27:30 +0900

    [test:part6] reactor request, doOn test

--- src/test/kotlin/me/shinyook/reactorguide/Part06RequestTest.kt
+++ src/test/kotlin/me/shinyook/reactorguide/Part06RequestTest.kt
@@ -1,5 +1,48 @@
 package me.shinyook.reactorguide
 
-import org.junit.jupiter.api.Assertions.*
+import me.shinyook.reactorguide.domain.User
+import me.shinyook.reactorguide.repository.UserRepository
+import org.junit.jupiter.api.Test
+import reactor.core.publisher.Flux
+import reactor.test.StepVerifier
 
-internal class Part06RequestTest
+internal class Part06RequestTest {
+
+    private val guideLine = Part06Request()
+    private val userRepository = UserRepository()
+
+    @Test
+    fun requestAll() {
+        val users = userRepository.findAll()
+        guideLine.requestAllExpectFour(users).verify()
+    }
+
+    @Test
+    fun requestOneByOne() {
+        val users = Flux.just(User.SKYLER, User.JESSE, User.MIKE)
+        guideLine.requestOneExpectSkylerThenRequestOneExpectJesse(users).verify()
+    }
+
+    @Test
+    fun fluxLog() {
+        val flux = guideLine.fluxWithLog()
+        StepVerifier.create(flux, 0)
+            .thenRequest(1)
+            .expectNextMatches { true }
+            .thenRequest(1)
+            .expectNextMatches { true }
+            .thenRequest(2)
+            .expectNextMatches { true }
+            .expectNextMatches { true }
+            .verifyComplete()
+    }
+
+    @Test
+    fun fluxWithDoOn() {
+        val flux = guideLine.fluxWithDoOnPrintln()
+        StepVerifier.create(flux)
+            .expectNextCount(4)
+            .verifyComplete()
+    }
+
+}
