079ce7f1df5148a391e159c02cabd7b9e57f2a45
leeshinyook <tlsdbr97@gmail.com>
2022-05-06 19:03:58 +0900

    [test:part9] reactive to blocking test

--- src/test/kotlin/me/shinyook/reactorguide/Part09ReactiveToBlockingTest.kt
+++ src/test/kotlin/me/shinyook/reactorguide/Part09ReactiveToBlockingTest.kt
@@ -1,5 +1,30 @@
 package me.shinyook.reactorguide
 
-import org.junit.jupiter.api.Assertions.*
+import me.shinyook.reactorguide.domain.User
+import me.shinyook.reactorguide.repository.UserRepository
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
 
-internal class Part09ReactiveToBlockingTest
+internal class Part09ReactiveToBlockingTest {
+
+    private val guideLine = Part09ReactiveToBlocking()
+    private val userRepository = UserRepository()
+
+    @Test
+    fun mono() {
+        val mono = userRepository.findFirst()
+        assertThat(guideLine.monoToValue(mono)).isEqualTo(User.JESSE)
+    }
+
+    @Test
+    fun flux() {
+        val flux = userRepository.findAll()
+        val users = guideLine.fluxToValues(flux)
+        val it = users.iterator()
+        assertThat(it.next()).isEqualTo(User.JESSE)
+        assertThat(it.next()).isEqualTo(User.SKYLER)
+        assertThat(it.next()).isEqualTo(User.SAUL)
+        assertThat(it.next()).isEqualTo(User.WALTER)
+        assertThat(it.hasNext()).isFalse
+    }
+}
