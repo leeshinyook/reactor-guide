e0c28fb692274cd95d9718123fc3299e265cf834
leeshinyook <tlsdbr97@gmail.com>
2022-05-06 19:53:33 +0900

    [test:part10] blocking to reactive test

--- src/test/kotlin/me/shinyook/reactorguide/Part10BlockingToReactiveTest.kt
+++ src/test/kotlin/me/shinyook/reactorguide/Part10BlockingToReactiveTest.kt
@@ -1,5 +1,34 @@
 package me.shinyook.reactorguide
 
-import org.junit.jupiter.api.Assertions.*
+import me.shinyook.reactorguide.domain.User
+import me.shinyook.reactorguide.repository.UserRepository
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import reactor.core.publisher.Mono
+import reactor.test.StepVerifier
 
-internal class Part10BlockingToReactiveTest
+internal class Part10BlockingToReactiveTest {
+
+    private val guideLine = Part10BlockingToReactive()
+    private var userRepository: UserRepository = UserRepository()
+
+
+    @Test
+    fun slowPublisherFastSubscriber() {
+        val flux = guideLine.blockingRepositoryToFlux(userRepository)
+        assertThat(userRepository.callCount).isZero
+        StepVerifier.create(flux)
+            .expectNext(User.JESSE, User.SKYLER, User.SAUL, User.WALTER)
+            .verifyComplete()
+        assertThat(userRepository.callCount).isOne
+    }
+
+    @Test
+    fun fastPublisherSlowSubscriber() {
+        val complete: Mono<Void> = guideLine.fluxToBlockingRepository(userRepository)
+        assertThat(userRepository.callCount).isEqualTo(1)
+        StepVerifier.create(complete)
+            .verifyComplete()
+        assertThat(userRepository.callCount).isEqualTo(5)
+    }
+}
